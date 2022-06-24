48a5b700ce997e50ae77217dd941da5fc5cf24f1
GianlorenzoDercole <johndercole01@gmail.com>
2022-06-24 15:51:19 -0700

    changes to navbar img moved images folder and deleted hello world

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import image from "../images/picitupfinal.png"
 export default function Navbar({ currentUser, handleLogout }) {
   const loggedIn = (
     <React.Fragment>
       <Link to='/'>
         <span onClick={handleLogout}>Log Out</span>
       </Link>
-      {' || '}
+
       <Link to='/profile'>Profile</Link>
+
     </React.Fragment>
   )
 
   const loggedOut = (
     <React.Fragment>
       <Link to='/register'>Register</Link>
-      {' || '}
+
+
     </React.Fragment>
   )
 
   return (
-    <nav className="bg-white text-white  border-gray-200 px-2 sm:px-10 py-2.5 rounded dark:bg-pink-800">
+    <nav className="bg-white   text-white border-gray-200 px-2 sm:px-10 py-2.5  dark:bg-gray-800">
       <div class="container flex flex-wrap justify-evenly items-center mx-auto">
-      <Link to='/'>User App</Link>{' || '}
+      <Link to='/'>User App</Link>
+      <Link to='/'>
+        <img width="150" src={image}/>
+      </Link>
 
 
       {currentUser ? loggedIn : loggedOut}
