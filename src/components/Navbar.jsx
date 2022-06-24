fcd44b25aa44c251bef8e3ed4195e0b33dd28bae
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-06-23 20:10:24 -0700

    REST routes added to readme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,28 @@ import { Link } from 'react-router-dom'
 export default function Navbar({ currentUser, handleLogout }) {
   const loggedIn = (
     <React.Fragment>
+
       <Link to='/'>
-        <span onClick={handleLogout}>Logout</span>
-      </Link>
+        <span onClick={handleLogout}>Log Out</span>
+      </Link>{' || '}
       <Link to='/profile'>Profile</Link>
+
     </React.Fragment>
   )
 
   const loggedOut = (
     <React.Fragment>
-      <Link to='/register'>Register</Link>
-      <Link to='/login'>Login</Link>
+
+      <Link to='/register'>Register</Link>{' || '}
+
+      <Link to='/login'>Log In</Link>
+
     </React.Fragment>
   )
+
   return (
     <nav>
-      <Link to='/'>User App</Link>
+      <Link to='/'>User App</Link>{' || '}
 
       {currentUser ? loggedIn : loggedOut}
     </nav>
