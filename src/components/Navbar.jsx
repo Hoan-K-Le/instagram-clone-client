50b6a2b395ca79fd864718ebc4f5cdaa5b0b34e9
GianlorenzoDercole <johndercole01@gmail.com>
2022-06-27 09:12:46 -0700

    changes to nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar({ currentUser, handleLogout }) {
 
   const loggedOut = (
     <React.Fragment>
-      <Link to='/register'>Register</Link>
+      <Link to='/register'><p class=" hover:text-red-400">Register</p></Link>
 
 
     </React.Fragment>
@@ -23,10 +23,10 @@ export default function Navbar({ currentUser, handleLogout }) {
 
   return (
     <nav className="bg-white   text-white border-gray-200 px-2 sm:px-10 py-2.5  dark:bg-gray-800">
-      <div class="container flex flex-wrap justify-evenly items-center mx-auto">
-      <Link to='/'>User App</Link>
+      <div class="  font-mono   container flex flex-wrap justify-evenly items-center mx-auto">
+      <Link to='/'><p class="  hover:text-red-400 ">User App</p></Link>
       <Link to='/'>
-        <img width="150" src={image}/>
+        <img width="150" src={image} alt="pic it up logo"/>
       </Link>
 
 
