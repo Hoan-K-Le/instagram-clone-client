7e33d8a02b352c58cdc87d2b0d8d5876039e4ea9
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-06-30 15:01:54 -0700

    Navbar profile pic fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,88 +1,16 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import image from '../images/picitupfinal.png'
-// import { useState } from 'react'
-
-// export default function Navbar({ currentUser, handleLogout }) {
-//   const [toggleDrop, setToggleDrop] = useState(false)
-
-//   const loggedIn = (
-//     <React.Fragment>
-//       <div className="mx-auto bg-gray-800 font-arial pt-1 pb-3 flex flex-wrap justify-between items-center m-0 w-screen px-5">
-//         <Link to="/">
-//           <img width="150" src={image} alt={image} />
-//         </Link>
-//         <button
-//           id="dropdownDefault"
-//           data-dropdown-toggle="dropdown"
-//           onClick={() => setToggleDrop(!toggleDrop)}
-//           className="relative text-white focus:ring-4 focus:outline-none focus:ring-gray-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-white-600 dark:hover:bg-white-700 dark:focus:ring-white-800"
-//           type="button"
-//         >
-//           {currentUser ? (
-//             <img
-//               className="w-12 h-12 rounded-full border"
-//               src={`https://res.cloudinary.com/dshcawt4j/image/upload/v1593119998/${currentUser.profilePicture}.png`}
-//               alt="user"
-//             />
-//           ) : (
-//             <img src="http://placekitten.com/200/300" alt="cats" />
-//           )}
-//           {''}
-//         </button>
-
-//         <div
-//           id="dropdown"
-//           className={
-//             toggleDrop
-//               ? `z-10 bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 absolute mt-8 top-10 right-8 `
-//               : `hidden z-10 bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 absolute top-20`
-//           }
-//         >
-//           <ul
-//             className="py-1 text-sm text-gray-700 dark:text-gray-200"
-//             aria-labelledby="dropdownDefault"
-//           >
-//             <li>
-//               <a
-//                 href="/profile"
-//                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-//               >
-//                 Profile
-//               </a>
-//             </li>
-//             <li>
-//               <a
-//                 href="/users"
-//                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-//               >
-//                 All Users
-//               </a>
-//             </li>
-//             <li className="hover:cursor-pointer">
-//               <span
-//                 onClick={handleLogout}
-//                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-//               >
-//                 Logout
-//               </span>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </React.Fragment>
-//   )
-//   return <React.Fragment>{currentUser ? loggedIn : null}</React.Fragment>
-// }
-
-
 import React from 'react'
 import { Link } from 'react-router-dom'
 import image from '../images/picitupfinal.png'
 import { useState } from 'react'
 
+
 export default function Navbar({ currentUser, handleLogout }) {
   const [toggleDrop, setToggleDrop] = useState(false)
+  const logOutAndCloseNav = () => {
+    setToggleDrop(!toggleDrop)
+    handleLogout()
+  }
+
   const loggedIn = (
     <React.Fragment>
       <div className='mx-auto bg-gray-800 font-arial pt-1 pb-3 flex flex-wrap justify-between items-center m-0 w-screen px-5'>
@@ -97,14 +25,22 @@ export default function Navbar({ currentUser, handleLogout }) {
           type='button'
         >
           {currentUser ? (
-            <img
-              className='w-12 h-12 rounded-full border'
-              src={`https://res.cloudinary.com/dshcawt4j/image/upload/v1593119998/${currentUser.profilePicture}.png`}
-              alt='user'
-            />
+            currentUser.profilePicture ? (
+              <img
+                className='w-12 h-12 rounded-full border'
+                src={`https://res.cloudinary.com/dshcawt4j/image/upload/v1593119998/${currentUser.profilePicture}.png`}
+                alt='user'
+              />
+            ) : (
+              <img
+                className='w-12 h-12 rounded-full border'
+                src='http://placekitten.com/200/300'
+                alt='cats'
+              />
+            )
           ) : (
-            <img src='http://placekitten.com/200/300' alt='cats' />
-          )}{' '}
+            <div>Loading...</div>
+          )}
         </button>
         <div
           id='dropdown'
@@ -136,11 +72,8 @@ export default function Navbar({ currentUser, handleLogout }) {
             </li>
             <li  className='hover:cursor-pointer'>
             <Link to='/' >
-              <span onClick={handleLogout}><p class='font-bold ml-4 pt-2 pb-1 transition duration-500 hover:text-blue-400'>Log Out</p></span>
+              <span onClick={logOutAndCloseNav}><p class='font-bold ml-4 pt-2 pb-1 transition duration-500 hover:text-blue-400'>Log Out</p></span>
             </Link>
-              {/* <span className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
-                Logout
-              </span> */}
             </li>
           </ul>
         </div>
--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -132,7 +132,7 @@ export default function Profile({
   }
 
   const modalButton = (
-    <button className='flex gap-x-3 row-reverse bg-purple-300 rounded-lg p-3 font-bold text-white' onClick={() => setModalToggle(!modalToggle)}>
+    <button className='shadow-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-0.5 hover:scale-110 hover:bg-pink-400 duration-300 flex gap-x-3 row-reverse bg-purple-400 rounded-lg p-3 font-bold text-white mt-2' onClick={() => setModalToggle(!modalToggle)}>
       Upload a picture <PhotographIcon className='h-5 w-5 text-white' />
     </button>
   )
@@ -164,7 +164,7 @@ export default function Profile({
       <div className='h-fit mt-10 bg-white flex flex-col justify-center items-center'>
         <div className='bg-gray-100 rounded-xl mb-5 border-gray-300 w-200 p-10 flex flex-col items-center shadow-lg'>
           <div className='bg-gray-200 mb-3 rounded-lg'>
-            <h1 className='font-bold p-3'>Hello, {name}</h1>
+            <h1 className='font-bold text-white p-3 px-20 dark:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg'>Hello, {name}</h1>
           </div>
           
 
@@ -206,9 +206,9 @@ export default function Profile({
           <h2 className='font-bold underline underline-offset-4'>Profile Options</h2>
           <div className='mt-3 grid grid-cols-2'>
             <Link to={`/profile/${id}`}>
-              <button className='bg-green-500 p-2 rounded-lg font-bold text-white'>Edit Profile</button>
+              <button className='shadow-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 duration-300 bg-green-500 p-2 rounded-lg font-bold text-white'>Edit Profile</button>
             </Link>
-              <button className='bg-red-500 p-2 rounded-lg font-bold text-white' onClick={handleDelete}>Delete Profile </button>
+              <button className='shadow-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300 bg-red-500 p-2 rounded-lg font-bold text-white' onClick={handleDelete}>Delete Profile </button>
           </div>
         </div>
       </div>
@@ -222,14 +222,15 @@ export default function Profile({
           setUserProfile={setUserProfile}
         />
       ) : null}
-
-      <div className='bg-gray-100 rounded-xl mb-3 mx-5 p-5 flex flex-col items-center shadow-lg'>
-        <h1 className='font-bold text-center underline underline-offset-8 mb-3'>
-          {' '}
-          Your Posts{' '}
-        </h1>
-        {modalButton}
+      <div className='flex flex-col items-center'>
+        <div className='box-content h-40 w-80 justify-center'>
+          <div className='bg-gray-100 rounded-xl mb-3 mx-5 p-5 flex flex-col items-center shadow-lg'>
+            <h1 className='font-bold text-center underline underline-offset-8 mb-3'>Your Posts</h1>
+            {modalButton}
+          </div>
+        </div>
       </div>
+      
 
       
       {allUserPictures}
